6cd40527267f749b0df9dbf316202c80d4e9ffb7
赵宏健 <757566833@qq.com>
2020-03-24 14:43:59 +0800

    Initial commit

---
+++ static.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const Koa = require('koa');
+const send = require('koa-send');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const app = new Koa();
+const router = new Router();
+app.use(cors({
+  origin: "*",
+  exposeHeaders: ["WWW-Authenticate", "Server-Authorization"],
+  // maxAge: 50000,
+  credentials: true,
+  allowMethods: ["GET", "PUT", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+  allowHeaders: ["Content-Type", "Authorization", "Accept"],
+}));
+// router.get()
+router.get(['/', '/**'], async (ctx) => {
+  await send(ctx, ctx.path, {
+    root: path.join(__dirname, 'static'),
+    maxAge: 365 * 24 * 60 * 60 * 1000
+  });
+})
+
+
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(7000);
